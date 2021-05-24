a5e1dca8b9cb679d551b5f618399d3ec56ee3d48
MihaiVatavu <66440300+MihaiVatavu@users.noreply.github.com>
2021-05-24 13:32:36 +0100

    Connect to db

--- utilities/conn.php
+++ utilities/conn.php
@@ -1,9 +1,22 @@
 <?php
+require('vendor/autoload.php');
 
-$conn = mysqli_connect('localhost','movieadmin','adminmovie1234','movies_app');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 
-if(!$conn){
-  echo 'Connection error'. mysqli_connect_error();
+$dotenv->load();
+
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $_ENV['SERVER'];
+$cleardb_username = $_ENV['USERNAME'];
+$cleardb_password = $_ENV['PASSWORD'];
+$cleardb_db = substr($_ENV['DB'], 1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+// $conn = mysqli_connect('localhost','movieadmin','adminmovie1234','movies_app');
+
+if (!$conn) {
+  echo 'Connection error' . mysqli_connect_error();
 }
-
-?>
